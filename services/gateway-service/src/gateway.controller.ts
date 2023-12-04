6707f08029f976b2e48a975786d6f48df2d4c0ac
kirillpolozenko <polozenko.kirill.job@gmail.com>
2023-12-04 23:30:02 +0200

    feat: add all  auth methods

--- services/gateway-service/src/gateway.controller.ts
+++ services/gateway-service/src/gateway.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body, HttpCode, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from './auth/dto/create-user.dto';
+import { LoginUserDto } from './auth/dto/login-user.dto';
+import { RefreshTokenDto } from './auth/dto/refresh-token.dto';
 
 @Controller('auth')
 export class GatewayController {
@@ -11,5 +13,22 @@ export class GatewayController {
   @HttpCode(201)
   async signup(@Body() dto: CreateUserDto) {
     return await this.clientAuth.send({ cmd: 'register' }, dto).toPromise();
+  }
+  @Post('/login')
+  @HttpCode(200)
+  async login(@Body() dto: LoginUserDto) {
+    return await this.clientAuth.send({ cmd: 'login' }, dto).toPromise();
+  }
+  @Post('/refresh-token')
+  @HttpCode(201)
+  async refreshToken(@Body() dto: RefreshTokenDto) {
+    return await this.clientAuth
+      .send({ cmd: 'refresh-token' }, dto)
+      .toPromise();
+  }
+  @Post('/logout')
+  @HttpCode(201)
+  async logout(@Body() dto: RefreshTokenDto) {
+    return await this.clientAuth.send({ cmd: 'logout' }, dto).toPromise();
   }
 }
